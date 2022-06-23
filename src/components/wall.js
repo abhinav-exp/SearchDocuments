efb3922dda6a91a838b837f62d29d8e344c3166c
abhinav-exp <kabhinav482@gmail.com>
2022-06-23 10:14:27 +0530

    react start

--- src/components/wall.js
+++ src/components/wall.js
@@ -1,6 +1,7 @@
 import './wall.css';
 // import GoogleLogin from 'react-google-login';
 import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 import { GoogleOAuthProvider } from '@react-oauth/google';
@@ -18,19 +19,42 @@ function MyCustomButton()
         }
     });
     return (
-        <button onClick={login}>
-            Sign in with Google 🚀{' '}
+        <button type="button" className="btn btn-primary towardsright" onClick={login}>
+            Signin with Google
         </button>
     )
 }
 
-
-function Wall()
-{    
-    console.log("breui");
+function SideBar()
+{
     return (
-        <div className="fullscreen">
-        <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
+        <div className = 'SideBar'>
+            <ul className="nav nav-pills flex-column mb-auto">
+            <li className="nav-item">
+                <a href="/" className="nav-link active" aria-current="page">
+                    Home
+                </a>
+            </li>
+            <li>
+            <a href="/" className="nav-link text-white">
+                Dashboard
+            </a>
+            </li>
+            <li>
+            <a href="/" className="nav-link text-white">
+                Orders
+            </a>
+            </li>
+            </ul>
+        </div>
+    )
+}
+
+function Header()
+{
+    return (
+        <div className='header'>
+            <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
             <MyCustomButton >
             </MyCustomButton>
             {/* <GoogleLogin        // commented code works for one tap
@@ -46,8 +70,45 @@ function Wall()
                     console.log('Login Failed');
                 }}
             />; */}
-        </GoogleOAuthProvider>;
-    </div>);
+            </GoogleOAuthProvider>
+        </div>
+    )
+}
+
+function SearchButton()
+{
+    return (
+        <div className='searchBar'>
+            <div className="input-group">
+                <input type="search" className="form-control rounded" placeholder="Search" aria-label="Search" aria-describedby="search-addon" />
+                <button type="button" className="btn btn-outline-primary">search</button>
+            </div>
+        </div>
+    )
+}
+
+function MainScreen()
+{
+    return (
+        <div className='MainScreen'>
+            <SearchButton></SearchButton>
+        </div>
+    )
+}
+
+function Wall()
+{    
+    return (
+        <div className="fullscreen">
+            <div className='leftpart'>
+                <SideBar></SideBar>
+            </div>
+            <div className='rightpart'>
+                <Header></Header>
+                <MainScreen></MainScreen>
+            </div>
+        </div>
+    );
 }
 
 export default Wall;
