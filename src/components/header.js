d28a155e88c233067f319eabaa84f5e091485242
abhinav-exp <kabhinav482@gmail.com>
2022-06-24 16:40:28 +0530

    routes and cookies

---
+++ src/components/header.js
@@ -0,0 +1,89 @@
+import axios from 'axios';
+import { GoogleOAuthProvider, useGoogleLogin } from '@react-oauth/google';
+import { Link } from "react-router-dom";
+
+function MyCustomButton(props)
+{
+    const login = useGoogleLogin({
+        onSuccess : tokenResponse => {
+            console.log(tokenResponse);
+            axios.post('http://localhost:8000/dj-rest-auth/google/', {
+                access_token: tokenResponse.access_token,
+            })
+            .then(r => {
+                props.CookieManager[1]('authcookie', r.data);
+            });
+        }
+    });
+    return (
+        <button type="button" className="btn btn-primary towardsright" onClick={login}>
+            Signin with Google
+        </button>
+    )
+}
+
+function MyGoogleSignIn(props)
+{
+    return (
+        <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
+            <MyCustomButton CookieManager = {props.CookieManager}>
+            </MyCustomButton>
+        </GoogleOAuthProvider>
+    )
+}
+
+function Logout(props)
+{
+    const logoutonclick = () => {
+        props.CookieManager[2]('authcookie');
+    }
+    return (
+        <div className='towardsright'>
+            <div className='Name-box'>
+                Welcome, {props.CookieManager[0]['authcookie']['user']['first_name']} {props.CookieManager[0]['authcookie']['user']['last_name']} 
+            </div>
+            <button className="btn btn-primary" onClick={logoutonclick}> Logout </button>
+        </div>
+    )
+}
+
+function NavBar(props)
+{
+    return (
+        <div className = 'NavBar'>
+            <ul class="nav">
+                <li class="nav-item">
+                    <Link className='nav-link' to="/">Home</Link>   
+                </li>
+                <li class="nav-item">
+                    <Link class="nav-link" to="/trends">Trends</Link>
+                </li>
+                {
+                    props.CookieManager[0]['authcookie'] !== undefined ?
+                    <li class="nav-item">
+                        <Link class="nav-link" to="/yours">Your Documents</Link>
+                    </li> : <div></div>
+                }
+                
+                {/* <li class="nav-item">
+                    <a class="nav-link disabled" href="/">Disabled</a>
+                </li> */}
+            </ul>
+        </div>
+    )
+}
+
+function Header(props)
+{
+    console.log(props)
+    return (
+        <div className='header'>
+            <NavBar CookieManager = {props.CookieManager}></NavBar>
+            {props.CookieManager[0]['authcookie'] === undefined? 
+                <MyGoogleSignIn CookieManager = {props.CookieManager}></MyGoogleSignIn> : <Logout CookieManager = {props.CookieManager}></Logout>
+            }
+        </div>
+    )
+}
+
+export default Header;
--- src/components/wall.js
+++ src/components/wall.js
@@ -1,79 +1,9 @@
 import './wall.css';
-// import GoogleLogin from 'react-google-login';
-import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-
-import { GoogleOAuthProvider } from '@react-oauth/google';
-import { useGoogleLogin } from '@react-oauth/google';
-// import { GoogleLogin } from '@react-oauth/google';
-function MyCustomButton()
-{
-    const login = useGoogleLogin({
-        onSuccess : tokenResponse => {
-            console.log(tokenResponse);
-            axios.post('http://localhost:8000/dj-rest-auth/google/', {
-                access_token: tokenResponse.access_token,
-            })
-            .then(r => {console.log(r.data.access_token)});
-        }
-    });
-    return (
-        <button type="button" className="btn btn-primary towardsright" onClick={login}>
-            Signin with Google
-        </button>
-    )
-}
-
-function SideBar()
-{
-    return (
-        <div className = 'SideBar'>
-            <ul className="nav nav-pills flex-column mb-auto">
-            <li className="nav-item">
-                <a href="/" className="nav-link active" aria-current="page">
-                    Home
-                </a>
-            </li>
-            <li>
-            <a href="/" className="nav-link text-white">
-                Dashboard
-            </a>
-            </li>
-            <li>
-            <a href="/" className="nav-link text-white">
-                Orders
-            </a>
-            </li>
-            </ul>
-        </div>
-    )
-}
-
-function Header()
-{
-    return (
-        <div className='header'>
-            <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
-            <MyCustomButton >
-            </MyCustomButton>
-            {/* <GoogleLogin        // commented code works for one tap
-                onSuccess={credentialResponse => {
-                    console.log(credentialResponse);
-                    let res = axios.post('http://localhost:8000/dj-rest-auth/google/', {
-                        id_token: credentialResponse.credential,
-                    });
-                    console.log(res);
-                    // return res.status;
-                }}
-                onError={() => {
-                    console.log('Login Failed');
-                }}
-            />; */}
-            </GoogleOAuthProvider>
-        </div>
-    )
-}
+import { CookiesProvider } from 'react-cookie';
+import { useCookies } from 'react-cookie';
+import Header from './header'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function SearchButton()
 {
@@ -87,7 +17,7 @@ function SearchButton()
     )
 }
 
-function MainScreen()
+function SearchScreen()
 {
     return (
         <div className='MainScreen'>
@@ -96,18 +26,46 @@ function MainScreen()
     )
 }
 
+function TrendScreen()
+{
+    return (
+        <div>
+            Trends
+        </div>
+    )
+}
+
+function MyScreen()
+{
+    return (
+        <div>
+            yours
+        </div>
+    )
+}
+
 function Wall()
 {    
+    const CookieManager = useCookies();
     return (
-        <div className="fullscreen">
-            <div className='leftpart'>
-                <SideBar></SideBar>
+        <CookiesProvider>
+            <div className="fullscreen">
+                {/* <div className='leftpart'>
+                    <SideBar CookieManager={CookieManager}></SideBar>
+                </div> */}
+                {/* <div className='rightpart'> */}
+                <BrowserRouter>
+                <Header CookieManager={CookieManager}></Header>
+                    <Routes>
+                        <Route path="/" element={<SearchScreen></SearchScreen>}></Route>
+                        <Route path="trends" element={<TrendScreen></TrendScreen>}></Route>
+                        <Route path="yours" element= {<MyScreen></MyScreen>}></Route>
+                    </Routes>
+                </BrowserRouter>
+                
+                {/* </div> */}
             </div>
-            <div className='rightpart'>
-                <Header></Header>
-                <MainScreen></MainScreen>
-            </div>
-        </div>
+        </CookiesProvider>
     );
 }
 
