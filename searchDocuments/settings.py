828e6f59e23cb6f33554eb3c6f187b265f2a2d9b
abhinav-exp <kabhinav482@gmail.com>
2022-07-10 12:17:03 +0530

    allowed host

--- searchDocuments/settings.py
+++ searchDocuments/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'ec2-3-86-190-138.compute-1.amazonaws.com', '3.86.190.138']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '23.22.169.156']
 
 
 # Application definition
